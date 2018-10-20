9d52ddbfb093e7bf9ca44895da708a2f003ca19a
Srihari <srihari@EPCBLR-050.evolvus.com>
2018-10-20 18:46:35 +0530

    Added Applications Dropdown

--- src/app/common-component/starter-header/starter-header.component.ts
+++ src/app/common-component/starter-header/starter-header.component.ts
@@ -1,8 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthenticationService } from './../../login-console/authentication/login/login.service';
 import { Component, OnInit } from '@angular/core';
 import { SandstormGlobalVariablesService } from '../../shared/sandstorm-global-variables.service';
-
+import { environment } from '../../../environments/environment';
+  
 @Component({
   selector: 'app-starter-header',
   templateUrl: './starter-header.component.html',
@@ -12,10 +14,23 @@ export class StarterHeaderComponent implements OnInit {
   body: HTMLBodyElement = document.getElementsByTagName('body')[0];
   user: any;
   loggingOut: boolean = false;
-  constructor(private authService: AuthenticationService, private globalVariableService: SandstormGlobalVariablesService, private router: Router) { }
+  platformURL = environment.platformURL;
+  listOfApplications: any[]=[];
+  application: any; 
+
+  constructor(private http: HttpClient,private authService: AuthenticationService, private globalVariableService: SandstormGlobalVariablesService, private router: Router) { }
 
   ngOnInit() {
+
     this.user = this.globalVariableService.currentUser.getValue();
+
+this.getApplicationData().subscribe((response: any)=>{
+  response.data.map((application)=>{
+    if(application.applicationName!='SANDSTORM'){
+      this.listOfApplications.push(application);
+    }
+  })
+})
   }
 
   sideBarmenu(){
@@ -34,6 +49,15 @@ logout(){
 }
 
 
+getApplicationData(){
+  return this.http.get(`${this.platformURL}/sandstorm/api/application`);
+  }
 
+
+  navToApp(application){ 
+
+      window.open(`http://${application.url}/redirect/${this.user.userId}/${localStorage.getItem("token")}`);  
+  
+   }
 
 }
